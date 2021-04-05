d547749c3ba354038b8b7ca959800a8999ceca04
Mariano <marianov994@gmail.com>
2021-04-05 03:41:59 -0300

    Icon Chat moved to icons folder

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import UserList from './UserList'
 import PropTypes from 'prop-types'
+
+import UserList from './UserList'
+import IconChat from './icons/IconChat';
 
 const Nav = ({ contacts }) => {
   return (
@@ -26,7 +28,7 @@ const NavTitle = ({ title }) => {
   return (
     <div className="flex flex-row items-center justify-center h-12 w-full">
       <div className="flex items-center justify-center rounded-2xl text-indigo-700 bg-indigo-100 h-10 w-10">
-        <ChatIcon />
+        <IconChat />
       </div>
       <div className="ml-2 font-bold text-2xl">
         { title }
@@ -37,25 +39,6 @@ const NavTitle = ({ title }) => {
 
 NavTitle.propTypes = {
   title: PropTypes.string.isRequired,
-}
-
-const ChatIcon = () => {
-  return (
-    <svg
-      className="w-6 h-6"
-      fill="none"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
-      ></path>
-    </svg>
-  )
 }
 
 export default Nav
