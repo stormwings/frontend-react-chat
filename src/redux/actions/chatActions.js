2e3a11e791388aae8cca6fc4130d1e7948bfba96
Mariano <marianov994@gmail.com>
2021-04-09 03:10:29 -0300

    redux new chatActions

---
+++ src/redux/actions/chatActions.js
@@ -0,0 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { urlChat } from '../urls';
+import Http from './../../utils/http';
+import * as types from '../types';
+
+export const fetchChats = (userId) => (dispatch) => {
+  dispatch({ type: types.FETCH_CHATS_PENDING });
+
+  Http.instance
+    .get(`${urlChat}/${userId}`)
+    .then(({ body }) => {
+      dispatch({
+        type: types.FETCH_CHATS_FULLFILLED,
+        payload: { chats: body },
+      });
+    })
+    .catch((error) => {
+      dispatch({
+        type: types.FETCH_CHATS_REJECTED,
+        payload: { error },
+      });
+    });
+};
+
+export const useChatsReducer = () => {
+  const { chatsReducer } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const getChats = (userId) => {
+    dispatch(fetchChats(userId));
+  };
+
+  const chatsActions = {
+    getChats,
+  };
+
+  return { chatsReducer, chatsActions };
+};
