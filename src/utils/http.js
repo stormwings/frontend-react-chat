6dfa2b008032de61fbce447548f910b175d22543
Mariano <marianov994@gmail.com>
2021-04-08 23:15:08 -0300

    http adapter pattern to requests

---
+++ src/utils/http.js
@@ -0,0 +1,55 @@
+export default class Http {
+  static instance = new Http();
+
+  get = async (url) => {
+    try {
+      const req = await fetch(url);
+      const json = await req.json();
+      return json;
+    } catch (error) {
+      console.log('HTTP GET Error: ', error);
+      throw Error(error);
+    }
+  };
+
+  post = async (url, body) => {
+    try {
+      const request = await fetch(url, {
+        method: 'POST',
+        body,
+      });
+      const json = await request.json();
+      return json;
+    } catch (error) {
+      console.error('HTTP POST Error: ', error);
+      throw Error(error);
+    }
+  };
+
+  put = async (url, body) => {
+    try {
+      const request = await fetch(url, {
+        method: 'PUT',
+        body,
+      });
+      const json = await request.json();
+      return json;
+    } catch (error) {
+      console.error('HTTP PUT Error: ', error);
+      throw Error(error);
+    }
+  };
+
+  remove = async (url) => {
+    try {
+      const request = await fetch(url, {
+        method: 'DELETE',
+      });
+      const json = await request.json();
+      return json;
+    } catch (error) {
+      console.error('HTTP DELETE Error: ', error);
+      throw Error(error);
+    }
+  };
+}
